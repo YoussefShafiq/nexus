86e56a1ff8547030ba37efa968d91c696009b8f7
Youssef Shafek <youssefmsa616@gmail.com>
2025-08-20 21:14:52 +0300

    fix: home hero section responsive

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,13 +6,13 @@ export function HeroSection() {
     return <>
         <div className="bg-homeHero bg-no-repeat bg-cover min-h-[700px] lg:pt-40 pt-32 pb-32 text-white">
             <div className="container flex items-center flex-col lg:flex-row gap-16 ">
-                <div className="w-1/2 flex flex-col gap-5">
+                <div className="lg:w-1/2 flex flex-col gap-5">
                     <h1 className='font-extrabold text-4xl'>NEXUS:  Where Expertise Meets Innovation</h1>
                     <p className='text-xl'>Delivering advanced, reliable engineering solutions for oil & gas, industrial, and residential projects.</p>
                     <p className='text-xl'>32+ years of experience</p>
                     <PrimaryButton text={'Explore Our Projects'} path={'/projects'} />
                 </div>
-                <div className="w-1/2 flex justify-center items-center">
+                <div className="lg:w-1/2 flex justify-center items-center">
                     <img src={heroImg} alt="" />
                 </div>
             </div>
